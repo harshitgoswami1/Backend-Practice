0a4d6fea5797370642395f394c99ccb3e342d238
Harshit Goswami <harshitgoswami@Harshits-MacBook-Air.local>
2026-02-06 15:47:37 +0530

    made it deployable

--- day9-deploying notes app/Backend/server.js
+++ day9-deploying notes app/Backend/server.js
@@ -2,11 +2,10 @@ require("dotenv").config()
 const connectToDb = require("./src/config/database")
 const app = require("./src/app")
 
-
-
+const PORT = process.env.PORT || 3000
 
 connectToDb()
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
